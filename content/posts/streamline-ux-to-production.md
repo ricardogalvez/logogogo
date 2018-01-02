f357827df1ed3fb45088eeb5352cfd3bdc2471f9
ricardo galvez <ricardogalvez@nypl.org>
2018-01-02 16:06:52 -0500

    :meat_on_bone: gulpfile updated and a new blog post

---
+++ content/posts/streamline-ux-to-production.md
@@ -0,0 +1,18 @@
+---
+date: 2018-01-02T12:54:30-05:00
+draft: false
+tags: ["ux," ,"visual design" ]
+title: "Streamline Ux to Production"
+---
+
+Kick off discussion on improving and streamlining our UX / Visual Design Concept & Front end development work flow.
+For consideration and discussion:
+
+* Wire framing / Very Low Fidelity product and concept development
+    - a11y & semantics
+    - use case scenarios
+    - persona considerations
+* Sketch Comps and Internal Product Documentation
+* Static screen development with Avocode
+* Toolkit Integration
+* Development to Production
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+const gulp = require('gulp');
+
+const sass = require('gulp-sass');
+
+const sourcemaps = require('gulp-sourcemaps');
+
+const autoprefixer = require('gulp-autoprefixer');
+
+//
+// variables:
+//
+const input = 'themes/blankblank/src/sass/**/*.scss';
+const output = 'themes/blankblank/static/css';
+
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+const autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+//
+// gulp tasks:
+//
+gulp.task('sass', function () {
+  return gulp.src(input)
+  .pipe(sass())
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(sourcemaps.init())
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('themes/blankblank/src/sass/**/*.scss', ['sass']);
+
+});
+
+gulp.task('default', ['sass', 'watch']);
